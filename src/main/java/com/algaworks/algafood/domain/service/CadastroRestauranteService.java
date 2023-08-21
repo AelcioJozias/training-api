a13bb49b822e8429c981d9436b3bbb0ea71e86aa
AelcioJozias <aelciojoziasputzel18@gmail.com>
2023-08-21 14:18:01 -0300

    [desafio-exceptions] - Refatorando os serviçoes rest
    usando exceptions com a anotação @responseStatus e deixando o código muito mais clean, pois não precisa ficar colocando vários try catch

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -4,30 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 @Service
 public class CadastroRestauranteService {
 
+
+	private static final String NÃO_EXISTE_UM_RESTAURANTE_COM_O_ID = "Não existe um restaurante com o id %d: ";
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 	
 	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	private CadastroCozinhaService cadastroCozinhaService;
 	
 	public Restaurante salvar(Restaurante restaurante) {
-		Long cozinhaId = restaurante.getCozinha().getId();
-		
-		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
-		
-		restaurante.setCozinha(cozinha);
-		
+		cadastroCozinhaService.buscarOuFalhar(restaurante.getCozinha().getId());
 		return restauranteRepository.save(restaurante);
 	}
 	
+	public Restaurante buscarOuFalhar(Long id) {
+		return restauranteRepository.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException(
+				String.format(NÃO_EXISTE_UM_RESTAURANTE_COM_O_ID, id)));
+	}
 }
