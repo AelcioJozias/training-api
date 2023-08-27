9bdf871a341de49937010493d1c97809eb95c16b
AelcioJozias <aelciojoziasputzel18@gmail.com>
2023-08-27 19:27:25 -0300

    [modalagem-exceptions] - implementando RFC7807 e problemType Enum
    Deletei os método de tratamento de exceção mais específicos, deixando todos irem para os mais genéricos que sabem lidar.
    Além disso, criei um enum para lidar com os tipos de exceções.
    Também mudei a estrutura da classe Problem para o padrão RCT 7807

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,51 +9,41 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.algaworks.algafood.domain.exception.CidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.NegocioException;
-import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(EntidadeNaoEncontradaException.class)
-  public ResponseEntity<?> tratarEntidadeNaoEncontrada(EntidadeNaoEncontradaException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException e, WebRequest request) {
+
+    HttpStatus status = HttpStatus.NOT_FOUND;
+
+    Problem problem = createProblemBuilder(status, ProblemType.ENTIDADE_NAO_ENCONTRADA, e.getMessage()).build();
+
+    return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
   }
 
   @ExceptionHandler(NegocioException.class)
-  public ResponseEntity<?> tratarNegocioException(NegocioException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+  public ResponseEntity<?> handleNegocioException(NegocioException e, WebRequest request) {
+
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+
+    Problem problem = createProblemBuilder(status, ProblemType.ERRO_NEGOCIO, e.getMessage()).build();
+
+    return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
   }
 
-  @ExceptionHandler(CidadeNaoEncontradaException.class)
-  public ResponseEntity<?> tratarCidadeNaoEncontrada(CidadeNaoEncontradaException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-  }
-
-  @ExceptionHandler(CozinhaNaoEncontradaException.class)
-  public ResponseEntity<?> tratarCozinhaNaoEncontradaException(CozinhaNaoEncontradaException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-  }
 
   @ExceptionHandler(EntidadeEmUsoException.class)
-  public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
-  }
+  public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+    HttpStatus status = HttpStatus.CONFLICT;
 
-  @ExceptionHandler(EstadoNaoEncontradoException.class)
-  public ResponseEntity<?> tratarEstadoNaoEncontradoException(EstadoNaoEncontradoException e, WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-  }
+    Problem problem = createProblemBuilder(status, ProblemType.ENTIDADE_EM_USO, e.getMessage()).build();
 
-  @ExceptionHandler(RestauranteNaoEncontradoException.class)
-  public ResponseEntity<?> tratarRestauranteNaoEncontradoException(RestauranteNaoEncontradoException e,
-      WebRequest request) {
-    return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
   }
 
   @Override
@@ -63,18 +51,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
       HttpStatus status, WebRequest request) {
 
     if (body == null) {
-      body = Problema.builder()
-          .dataErro(LocalDateTime.now())
-          .menssagem(status.getReasonPhrase())
+      body = Problem.builder()
+          .title(status.getReasonPhrase())
+          .status(status.value())
           .build();
     } else if (body instanceof String) {
-      body = Problema.builder()
-          .dataErro(LocalDateTime.now())
-          .menssagem((String) body)
+      body = Problem.builder()
+          .title((String) body)
+          .status(status.value())
           .build();
     }
 
     return super.handleExceptionInternal(ex, body, headers, status, request);
+  }
+
+  private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail){
+    return Problem
+      .builder()
+      .status(status.value())
+      .title(problemType.getTitle())
+      .type(problemType.getUri())
+      .detail(detail);
   }
 
 }
