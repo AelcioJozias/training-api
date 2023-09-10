b8dae66787b1bc1dcb9faa6f44b7010cc4bda63f
Aelcio Jozias <aelciojoziasputzel18@gmail.com>
2023-09-10 02:22:45 -0300

    [bean-validation] - configurando o bundle do javax para usar o do spring
    adicionado um bean de configuração no projeto para que o resource bundle do bean validation seja o mesmo do spring, usando apenas o arquivo messages.properties para criar as mensagens do bean validation

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -26,7 +26,7 @@ import javax.validation.groups.Default;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.algaworks.algafood.Groups;
+import com.algaworks.algafood.core.validation.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -42,11 +42,11 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank()
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
-	@PositiveOrZero(message = "{TaxaFrete.invalida}")
+	@PositiveOrZero
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
