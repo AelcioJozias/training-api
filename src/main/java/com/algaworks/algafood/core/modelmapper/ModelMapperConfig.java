9454f9616be0af932e2d3a7d5b2d17ba6049cd69
Aelcio Pereira <aelciojoziasputzel18@gmail.com>
2023-11-14 01:49:53 -0300

    [modelagem-avancada]-criando emissao de pedido

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,7 +1,10 @@
 package com.algaworks.algafood.core.modelmapper;
 
-import com.algaworks.algafood.api.dto.EnderecoDTO;
+import com.algaworks.algafood.api.dto.input.ItemPedidoInput;
+import com.algaworks.algafood.api.dto.input.RestauranteInputDTO;
 import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.ItemPedido;
+import com.algaworks.algafood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,26 +13,18 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
 
     @Bean
-    public ModelMapper modelMapper(){
-        ModelMapper modelMapper = new ModelMapper();
+    public ModelMapper modelMapper() {
+        var modelMapper = new ModelMapper();
 
-
-
-        /*
-        * Todo o código abaixo nao funciona nas versoes mais nova de java
-        *  seta no @JsonSetter o nome da propriedade que é mais fácil
-        * */
-        // código abaixo serve para fazer um mapeamento manual, usando o o modelMapper
-
-        // cria a relacao entre o que você quer mapear
-//        @Deprecated
-//        var enderecoToEnderecoDTOTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class).include(Endereco);
+        modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+                .addMappings(mapper -> mapper.skip(ItemPedido::setId));
+//        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+//                Endereco.class, EnderecoModel.class);
 //
-//        // faz o mapeamento de acordo com valor que você fornece pela expressão lambda
-//        enderecoToEnderecoDTOTypeMap.<String>addMapping(src -> src.getCidade().getEstado().getNome(),
-//                (dest, value) -> dest.getCidade().setNomeEstado(value));
-
-
+//        enderecoToEnderecoModelTypeMap.<String>addMapping(
+//                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+//                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
         return modelMapper;
     }
+    
 }
