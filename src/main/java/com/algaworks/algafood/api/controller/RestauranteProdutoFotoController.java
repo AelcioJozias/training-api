6f9613ae4a6f980e0fa5de41effc45024eb1773d
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-01-02 01:09:18 -0300

    ..

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.api.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+
+public class RestauranteProdutoFotoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, MultipartFile arquivo) {
+
+        String nomeArquivo = UUID.randomUUID() + "_" + arquivo.getOriginalFilename();
+
+        Path arquivoFoto = Path.of("/home/aelcio.pereira@jtech.corp/catalogo", nomeArquivo);
+
+        System.out.println(arquivoFoto);
+        System.out.println(arquivo.getContentType());
+
+        try {
+            arquivo.transferTo(arquivoFoto);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
