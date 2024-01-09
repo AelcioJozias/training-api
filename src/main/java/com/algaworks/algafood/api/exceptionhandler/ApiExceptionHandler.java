302987add04ff1c9ffb6c889365cf81f17368f9f
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-01-09 01:16:26 -0300

    [foto-produto] - Corrigindo_exception_handler_de_media_type_não_aceita

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -54,6 +55,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         .userMassege(detail)
         .build();
     return handleExceptionInternal(ex, bodyResponse, headers, HttpStatus.INTERNAL_SERVER_ERROR, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    return ResponseEntity.status(status).headers(headers).build();
   }
 
   @Override
