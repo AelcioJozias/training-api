4abeef474c5faabf958ea5cdfabf4f8f264f2da9
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-01-13 18:50:35 -0300

    [mail-sender] - envia um email após confirmar um pedido

--- src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
@@ -1,8 +1,8 @@
 package com.algaworks.algafood.domain.service;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public interface EnvioEmailService {
@@ -11,9 +11,19 @@ public interface EnvioEmailService {
 
     @Getter
     @Setter
+    @Builder
     class Menssagem {
-        private Set<String> destinatarios;
+
+        // adicionar como se fosse um add do set ou array para não precisar instanciar uma lista se quiser passar apenas um destinario
+        @NonNull
+        @Singular
+        private Set<String> destinatarios = new HashSet<>();
+
+        // se não for passado um valor na instanciacao, será jogada uma exception
+        @NonNull
         private String assunto;
+
+        @NonNull
         private String corpo;
     }
 
--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -9,13 +9,25 @@ import com.algaworks.algafood.domain.model.Pedido;
 @Service
 public class FluxoPedidoService {
 
-  @Autowired
+    @Autowired
 	private EmissaoPedidoService emissaoPedido;
-	
+
+    @Autowired
+    private EnvioEmailService envioEmailService;
+
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+
+        var menssagem = EnvioEmailService.Menssagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("O pedido de código <strong>"
+                        + pedido.getCodigo() + "</strong> foi confirmado!")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmailService.enviar(menssagem);
 	}
 
   @Transactional
