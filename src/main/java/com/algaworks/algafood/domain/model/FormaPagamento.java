df0d330b13e5f37e53b4295874950d66fe51f685
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-01-19 14:39:49 -0300

    [novo-atributo-forma-pagamento]-adicionado data_atualizacao
    também foi criado um query para pegar o max value da última data de atualização

--- src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -8,6 +8,9 @@ import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -21,5 +24,9 @@ public class FormaPagamento {
 
 	@Column(nullable = false)
 	private String descricao;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private OffsetDateTime dataAtualizacao;
 
 }
--- src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -3,7 +3,13 @@ package com.algaworks.algafood.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.OffsetDateTime;
 
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+
+    @Query("select max(f.dataAtualizacao) from FormaPagamento f")
+    OffsetDateTime getUltimaAtualizacaoDataPagamento();
 
 }
