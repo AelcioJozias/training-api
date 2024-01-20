c1167941e8a7bbfaf5fd87a8566099f64d0d9ffa
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-01-19 22:19:40 -0300

    [doc]-selecionando apenas endpoints da aplicação para serem documentados

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -27,7 +28,10 @@ public class SpringFoxConfig implements WebMvcConfigurer {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 // indica quais endpoins dever sem documentados
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+                .paths(PathSelectors.any()) // -> esse já o valor padrão, está aqui apenas para referência
+                // no exemplo abaixo pegaria apenas os endpoints que começacem com restaurantes
+                //.paths(PathSelectors.regex("/restaurantes"))
                 .build();
     }
 
