506fab84e3b22f5697e2512af079c3c6c5c089e1
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-02-15 05:29:40 -0300

    [doc] - ajuste na order de exibição das pageds

--- src/main/java/com/algaworks/algafood/openapi/dto/PagedDtoOpenApi.java
+++ src/main/java/com/algaworks/algafood/openapi/dto/PagedDtoOpenApi.java
@@ -11,15 +11,15 @@ import java.util.List;
 public class PagedDtoOpenApi<T> {
     private List<T> content;
 
-    @ApiModelProperty(example = "10", value = "Quantidade de registros por página")
+    @ApiModelProperty(example = "10", value = "Quantidade de registros por página", position = 10)
     private Long size;
 
-    @ApiModelProperty(example = "50", value = "Total de registros")
+    @ApiModelProperty(example = "50", value = "Total de registros", position = 20)
     private Long totalElements;
 
-    @ApiModelProperty(example = "5", value = "Total de páginas")
+    @ApiModelProperty(example = "5", value = "Total de páginas", position = 30)
     private Long totalPages;
 
-    @ApiModelProperty(example = "0", value = "Número da página (começa em 0)")
+    @ApiModelProperty(example = "0", value = "Número da página (começa em 0)", position = 40)
     private Long number;
 }
