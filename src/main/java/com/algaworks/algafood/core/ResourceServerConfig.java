270ef80f104689c537d24a9851677f1c73f55fc9
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-03-13 07:54:50 -0300

    [security] - habilitando o cors no spring security

--- src/main/java/com/algaworks/algafood/core/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/ResourceServerConfig.java
@@ -14,6 +14,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
+                .cors()
+                .and()
                 .oauth2ResourceServer().opaqueToken();
     }
 
